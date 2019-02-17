4449767973990f982174ca9d6977c9eb28e4bcaa
Packia Sheela Varadhan <packiasheelav@gmail.com>
2019-02-17 13:13:31 +0200

    links modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AllRoutes from './components/AllRoutes';
-import NavBar from './components/NavBar';
-
+import React, { Component } from "react";
+import AllRoutes from "./components/AllRoutes";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
-	render() {
-		return (
-			<div>
-			<NavBar />
-			<AllRoutes />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <AllRoutes />
+      </div>
+    );
+  }
 }
 
 export default App;
